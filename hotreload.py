3849008f9f1ec827d75bead62882f393e2b0b1d7
shankarjp <shankarjp12@gmail.com>
2021-08-26 14:10:08 +0530

    detect changes to node done

--- hotreload.py
+++ hotreload.py
@@ -1,12 +1,19 @@
 import os
-import time
 
 last_mod = 0
+array = []
+for filepath in os.listdir('./node'):
+    if(os.path.isdir(os.path.join('./node',filepath))):
+        for files in os.listdir(os.path.join('./node',filepath)):
+            array.append(os.stat(os.path.join('./node',filepath, files)).st_mtime)
+    else:
+        array.append(os.stat(os.path.join('./node',filepath)).st_mtime)
+result = max(array)
+
 try:
     while True:
-        result = os.stat('./node/server.js')
-        if(result.st_mtime > last_mod):
-            last_mod = result.st_mtime
+        if(result > last_mod):
+            last_mod = result
             print("Restarting Docker Containers...")
             os.system('sudo docker-compose down')
             os.system('sudo docker rmi -f delta-onsite-hotreload_node')
